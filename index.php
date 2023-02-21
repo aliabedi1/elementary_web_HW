ad6514acdae3c6a9a45cd7f91fac547dd92ebd83
ali abedi <ali.abedi9731@yahoo.com>
2023-02-21 18:36:06 +0330

    this project is an edit on another project (homework) that i changed it for showing images in it

---
+++ index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Children's Museums</title>
+	<meta name="author" content="1Li" />
+	<!-- Date: 2020-11-14 -->
+	<link type="text/css" rel="stylesheet" href="assets/css/1.css" />
+	<link type="text/css" rel="stylesheet" href="assets/css/fonts.css" />
+</head>
+<body>
+	<div class="main_page_border">
+		<div class="main_page">
+			<div class="header">
+				<div class="neveste_bozorg_bala">
+					<span>FAKE<br/>
+					Adidas</span>
+				</div>
+				<div class="search_bar">
+					<div class="search_bar_div">
+						<input type="text" placeholder="جستجو" />
+					</div>
+					<div class="search_button_div">
+						<input type="button" value="برو" />
+					</div>
+				</div>
+			</div>
+			<div class="top_menu_bar">
+				<div class="menu_bar">
+					<div class="menu_bar_div">
+						<div>
+							<a href=""><span>درباره ما</span></a>
+						</div>
+					</div>
+					<div class="menu_bar_div">
+						<div>
+							<a href=""><span>وکالت نامه</span></a>
+						</div>
+					</div>
+					<div class="menu_bar_div">
+						<div>
+							<a href=""><span>داده ها و منابع</span></a>
+						</div>
+					</div>
+					<div class="menu_bar_div">
+						<div>
+								<a href=""><span>توسعه حرفه ای</span></a>
+						</div>
+					</div>
+					
+				</div>
+				<div class="bottom_of_menu_bar"></div>
+			</div>
+			<div class="main_menu_div">
+				<div class="borderaround">
+					<?php 
+					for ($i=1; $i <= 12; $i++) { 
+						echo '<div class="divamodi">
+							<img class="imageusingphp" src="assets/img/'.$i.'.jpg" />
+						</div>';
+					}
+					?>
+					
+				</div>
+			</div>
+			<div class="footer">
+				<div class="footer_header">
+					<div class="little_menu">
+						<div class="small_divs">
+							<span><a href="">خانه</a></span>
+						</div>
+						<div class="small_divs">
+							<span><a href="">ورود اعضا</a></span>
+						</div>
+						<div class="small_divs">
+							<span><a href="">تماس</a></span>
+						</div>
+					</div>
+					<div class="code_posti">
+						<span class="yellow_span">کد پستی:</span> <span class="gray_span">123456789101</span>
+					</div>
+					<div class="email">
+						<span class="yellow_span">ایمیل:</span> <span class="gray_span">email@yahoo.com</span>
+					</div>
+				</div>
+				<div class="footer_header_2"></div>
+				<div class="footer_div_sides_rast">
+					<div class="vertical_image">
+						<img src="assets/img/ftr-2.gif" />
+					</div>
+					<div class="vertical_menu">
+						<div class="vertical_menu_parts">
+							<a href=""><span>توسعه حرفه ای</span></a>
+						</div>
+						<div class="vertical_menu_parts">
+							<a href=""><span>داده ها و منابع</span></a>
+						</div>
+						<div class="vertical_menu_parts">
+							<a href=""><span>وکالت نامه</span></a>
+						</div>
+						<div class="vertical_menu_parts">
+							<a href=""><span>درباره ما</span></a>
+						</div>
+					</div>
+				</div>
+				<div class="footer_div_mid">
+					<div class="image_button_mid">
+						<div class="image_bar">
+							<img src="assets/img/communit-conversation.gif" />
+						</div>
+						<div class="button_bar">
+							<input type="button" value="وصل شو"/>
+						</div>
+					</div>
+					<div class="dotted_text">
+						<span>به گفتگوی انجمن بپیوندید</span>
+					</div>
+					<div class="normal_text">
+						<span>از کنفرانس های تلفنی تا بحث های آنلاین و تجربیات 
+							اینترنتی مجموعه کودکان در کنار شماست.</span>
+					</div>
+				</div>
+				<div class="footer_div_sides_chap">
+					<div class="only_margin">
+						<div class="logo_image">
+							<img src="assets/img/fb twitter.jpg" />
+						</div>
+						<div class="eng_text">
+							<span>© copyright 2013 association of
+								 Children's Museums. all rights reserved.</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+</body>
+</html>
+
+
